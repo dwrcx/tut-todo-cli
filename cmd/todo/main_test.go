8fc91c2842ab17308dbb3310227d835dab22faea
dw <dwrc.dev@gmail.com>
2025-03-06 13:41:56 +1100

    feat(cli): Add CLI for managing TODOs (tutorial snapshot)
    This commit captures the final implementation of the tutorial before
    the exercises.

--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -1,1 +1,84 @@
 package main_test
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"testing"
+)
+
+var (
+	binName  = "todo"
+	fileName = ".todo.json"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("Building tool...")
+
+	if runtime.GOOS == "windows" {
+		binName += ".exe"
+	}
+
+	build := exec.Command("go", "build", "-o", binName)
+
+	if err := build.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot build tool %s: %s", binName, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("running tests...")
+	result := m.Run()
+
+	fmt.Println("cleaning up...")
+	os.Remove(binName)
+	os.Remove(fileName)
+
+	os.Exit(result)
+}
+
+func TestTodoCLI(t *testing.T) {
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmdPath := filepath.Join(dir, binName)
+
+	task := "test task number 1"
+	t.Run("AddNewTaskFromArguments", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-add", task)
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	task2 := "test task number 2"
+	t.Run("AddNewTaskFromSTDIN", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-add")
+		cmdStdIn, err := cmd.StdinPipe()
+		if err != nil {
+			t.Fatal(err)
+		}
+		io.WriteString(cmdStdIn, task2)
+		cmdStdIn.Close()
+
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("ListTasks", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-list")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected := fmt.Sprintf("  1: %s\n  2: %s\n", task, task2)
+		if expected != string(out) {
+			t.Errorf("expected %q, got %q instead\n", expected, string(out))
+		}
+	})
+}
