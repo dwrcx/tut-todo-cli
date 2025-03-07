e8b69df08144f371e8c125b6488ad03d082d314f
dw <dwrc.dev@gmail.com>
2025-03-07 22:51:43 +1100

    refactor(cli): Add long flags for all commands
    - Added long-form, readable flags for all commands
    - Maintains short flags

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -13,14 +13,48 @@ import (
 var todoFileName = ".todo.json"
 
 func main() {
-	add := flag.Bool("a", false, "Add a new task")
-	list := flag.Bool("l", false, "List tasks")
-	verbose := flag.Bool("v", false, "Use with -l for verbose list output")
-	filterComplete := flag.Bool("fc", false, "Use with -l to filter complete tasks")
-	done := flag.Int("d", 0, "Mark task as done")
-	undone := flag.Int("ud", 0, "Revert (undo) a completed task")
-	remove := flag.Int("rm", 0, "Remove task")
+	add := flag.Bool("add", false, "Add a new task")
+	a := flag.Bool("a", false, "Alias for -add")
+
+	remove := flag.Int("remove", 0, "Remove task")
+	rm := flag.Int("rm", 0, "Alias for -remove")
+
+	done := flag.Int("done", 0, "Mark task as done")
+	d := flag.Int("d", 0, "Alias for -done")
+
+	undo := flag.Int("undo", 0, "Revert (undo) a completed task")
+	ud := flag.Int("ud", 0, "Alias for -undo")
+
+	list := flag.Bool("list", false, "List tasks")
+	ls := flag.Bool("l", false, "Alias for -list")
+
+	verbose := flag.Bool("verbose", false, "Use with -list for verbose list output")
+	v := flag.Bool("v", false, "Alias for -verbose")
+
+	filter := flag.Bool("filter", false, "Use with -list to filter complete tasks")
+	f := flag.Bool("f", false, "Alias for -filter")
+
 	clear := flag.Bool("clear", false, "Remove all tasks")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"Usage: todo [command] [options]\n\n"+
+				"Options:\n"+
+				"  -a,  -add        Add a new task\n"+
+				"  -rm, -remove N   Remove task N\n"+
+				"  -d,  -done N     Mark task N as done\n"+
+				"  -ud, -undo N     Revert completed task N\n"+
+				"  -l,  -list       List tasks\n"+
+				"  -v,  -verbose    Show detailed task info\n"+
+				"  -f,  -filter     Filter out completed tasks\n"+
+				"  -clear           Remove all tasks\n"+
+				"  -h   --help      Show this help message\n\n"+
+				"Examples:\n"+
+				"  todo -a Water plants\n"+
+				"  todo -done 1\n"+
+				"  todo -l -v -f (list verbose and filter completed items)")
+	}
+
 	flag.Parse()
 
 	if os.Getenv("TODO_FILENAME") != "" {
@@ -31,16 +65,16 @@ func main() {
 	loadTasks(l, todoFileName)
 
 	switch {
-	case *list:
-		printTasks(*l, *verbose, *filterComplete)
+	case *list || *ls:
+		printTasks(*l, *verbose || *v, *filter || *f)
 
-	case *done > 0:
-		modifyTask(l.Complete, *done, "Completed", l)
+	case *done > 0 || *d > 0:
+		modifyTask(l.Complete, getHighVal(*done, *d), "Completed", l)
 
-	case *undone > 0:
-		modifyTask(l.UndoComplete, *undone, "Reverted", l)
+	case *undo > 0 || *ud > 0:
+		modifyTask(l.UndoComplete, getHighVal(*undo, *ud), "Reverted", l)
 
-	case *add:
+	case *add || *a:
 		t, err := getTask(os.Stdin, flag.Args()...)
 
 		if err != nil {
@@ -52,8 +86,8 @@ func main() {
 		saveTasks(l, todoFileName)
 		fmt.Printf("Added Task [%s]\n", t)
 
-	case *remove > 0:
-		modifyTask(l.Delete, *remove, "Removed", l)
+	case *remove > 0 || *rm > 0:
+		modifyTask(l.Delete, getHighVal(*remove, *rm), "Removed", l)
 
 	case *clear:
 		l.DeleteAll()
@@ -62,8 +96,15 @@ func main() {
 
 	default:
 		fmt.Fprintf(os.Stderr, "Invalid option. Use -h for help.\n\n")
-		showHelp()
+		flag.Usage()
 	}
+}
+
+func getHighVal(num1, num2 int) int {
+	if num1 != 0 {
+		return num1
+	}
+	return num2
 }
 
 func loadTasks(l *todo.List, filename string) {
@@ -78,23 +119,6 @@ func saveTasks(l *todo.List, filename string) {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-}
-
-func showHelp() {
-	fmt.Println(`Usage: todo [command] [options]
-
-Commands:
-  a     "task"    Add a new task
-  rm    <number>  Remove a task
-  clear           Remove all tasks
-  d     <number>  Mark a task as done
-  ud    <number>  Revert (undo) a completed task
-  l               List tasks
-  l -v            List tasks verbose
-  l -fc           List tasks and filter complete items
-
-Options:
-  -h, --help      Show this help message`)
 }
 
 func getTask(r io.Reader, args ...string) (string, error) {
